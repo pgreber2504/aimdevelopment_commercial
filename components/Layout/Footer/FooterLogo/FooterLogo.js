c759ad17132ce90541d8b627b8c7076b18e588f0
pgreber2504 <p.greber2504@gmail.com>
2022-05-28 13:45:36 +0200

    adjusted font sizes

--- components/Layout/Footer/FooterLogo/FooterLogo.js
+++ components/Layout/Footer/FooterLogo/FooterLogo.js
@@ -8,13 +8,12 @@ const FooterLogo = () => {
     return (
         <Fragment>
             <div>
-                <img src='/images/icon.png' alt="Logo" className={classes["footer__logo"]} />
+                <img className={classes["footer__logo"]} />
             </div>
             <p className={classes["footer__copyright"]}>
                 &copy; Copyright by <Link
                     href="https://github.com/pgreber2504">
                     Przemysław Greber</Link>.
-                Dont claim as your own product.
             </p>
         </Fragment>
     )
