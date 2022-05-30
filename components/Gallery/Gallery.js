54306bcefb31f6a910b4e4a323236ca2812a4086
pgreber2504 <p.greber2504@gmail.com>
2022-05-30 19:48:20 +0200

    improved gallery data

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,35 +1,10 @@
-import React, { useState, useEffect, useCallback, Fragment } from 'react'
+import React, { useState, useCallback, Fragment } from 'react'
 import { wrap } from "popmotion";
-import classes from './Gallery.module.scss';
-import GalleryItem from './GalleryItem/GalleryItem';
-import Example from './GalleryItem/GalleryItem';
-import { AnimatePresence } from 'framer-motion';
+
 import GalleryDesktop from './GalleryDesktop/GalleryDesktop';
 import GalleryMobile from './GalleryMobile.js/GalleryMobile';
+import { GALLERY_DATA } from '../../constants/gallery-data';
 
-import Image1 from '../../public/images/gallery/img-1.webp';
-import Image2 from '../../public/images/gallery/img-2.webp';
-import Image3 from '../../public/images/gallery/img-3.webp';
-import Image4 from '../../public/images/gallery/img-4.webp';
-import Image5 from '../../public/images/gallery/img-5.webp';
-import Image6 from '../../public/images/gallery/img-6.webp';
-import Image7 from '../../public/images/gallery/img-7.webp';
-import Image8 from '../../public/images/gallery/img-8.webp';
-import Image9 from '../../public/images/gallery/img-9.webp';
-import Image10 from '../../public/images/gallery/img-10.webp';
-
-const GALLERY_DATA = [
-    { imgSrc: Image1, alt: 'Photo 1' },
-    { imgSrc: Image2, alt: 'Photo 2' },
-    { imgSrc: Image3, alt: 'Photo 3' },
-    { imgSrc: Image4, alt: 'Photo 4' },
-    { imgSrc: Image5, alt: 'Photo 5' },
-    { imgSrc: Image6, alt: 'Photo 6' },
-    { imgSrc: Image7, alt: 'Photo 7' },
-    { imgSrc: Image8, alt: 'Photo 8' },
-    { imgSrc: Image9, alt: 'Photo 9' },
-    { imgSrc: Image10, alt: 'Photo 10' },
-]
 
 
 const Gallery = () => {
@@ -41,17 +16,6 @@ const Gallery = () => {
     const paginate = useCallback((newDirection) => {
         setPage([page + newDirection, newDirection]);
     }, [setPage, page]);
-
-
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         paginate(1)
-    //     }, 7000)
-
-    //     return () => {
-    //         clearTimeout(timer)
-    //     }
-    // }, [paginate])
 
     return (
         <Fragment>
---
+++ constants/gallery-data.js
@@ -0,0 +1,21 @@
+import Image1 from '../public/images/gallery/1.webp';
+import Image2 from '../public/images/gallery/2.webp';
+import Image3 from '../public/images/gallery/3.webp';
+import Image4 from '../public/images/gallery/4.webp';
+import Image5 from '../public/images/gallery/5.webp';
+import Image6 from '../public/images/gallery/6.webp';
+import Image7 from '../public/images/gallery/7.webp';
+import Image8 from '../public/images/gallery/8.webp';
+import Image9 from '../public/images/gallery/9.webp';
+
+export const GALLERY_DATA = [
+    { imgSrc: Image1, alt: 'Photo 1' },
+    { imgSrc: Image2, alt: 'Photo 2' },
+    { imgSrc: Image3, alt: 'Photo 3' },
+    { imgSrc: Image4, alt: 'Photo 4' },
+    { imgSrc: Image5, alt: 'Photo 5' },
+    { imgSrc: Image6, alt: 'Photo 6' },
+    { imgSrc: Image7, alt: 'Photo 7' },
+    { imgSrc: Image8, alt: 'Photo 8' },
+    { imgSrc: Image9, alt: 'Photo 9' },
+]
