8cf735e710d64f21fe2a123e841e378fce7f0382
pgreber2504 <p.greber2504@gmail.com>
2022-05-30 14:35:55 +0200

    finished products component

--- components/Products/ProductsItem/ProductPhotos/ProductPhotos.js
+++ components/Products/ProductsItem/ProductPhotos/ProductPhotos.js
@@ -2,13 +2,13 @@ import Image from 'next/image'
 import React from 'react'
 import classes from './ProductPhotos.module.scss';
 
-import Photo from '../../../../public/images/offer/ikadom-producent-budynkow-modulowych-2021.jpeg';
+import Photo from '../../../../public/images/offer/1.jpeg';
 import ArrowButton from '../../../UI/ArrowButton/ArrowButton';
 
 const ProductPhotos = () => {
     return (
         <div className={classes['product--photos']}>
-            <Image src={Photo} alt='something' layout='fill' />
+            <Image src={Photo} alt='something' layout='fill' placeholder='blur' />
             <ArrowButton direction={'left'} />
             <ArrowButton direction={'right'} />
         </div>
---
+++ constants/products-data.js
@@ -0,0 +1,36 @@
+export const PRODUCTS_DATA = [
+    {
+        id: 'pr1',
+        yardage: '35m2',
+        name: 'Dom prostokąt',
+        description: 'Churchill to dom modułowy o powierzchni zabudowy 34,91m2. Mimo niewielkich rozmiarów mieści w sobie dwie sypialnie, salon z aneksem kuchennym oraz łazienkę. Jest to idealne rozwiązanie dla zapracowanych, zmęczonych miejskim zgiełkiem osób chcących wypocząć w zaciszu swojego ogródka działkowego. Moduł Churchill ze względu na swój kształt idealnie sprawdzi się w przypadku długich i wąskich działek.',
+        minPrice: 65000,
+        photos: []
+    },
+    {
+        id: 'pr2',
+        yardage: '35m2',
+        name: 'Dom L-ka',
+        description: 'Classico to domek modułowy o powierzchni zabudowy 34,69m2. Mimo niewielkich rozmiarów mieści w sobie dwie sypialnie, salon z aneksem kuchennym oraz łazienkę. Jest to idealne rozwiązanie dla zapracowanych, zmęczonych miejskim zgiełkiem osób chcących wypocząć w zaciszu swojego ogrodu. Marzą Państwo o filiżance ciepłej kawy na kameralnym tarasie o poranku czy zachodzie słońca? Ten domek jest spełnieniem tych marzeń! Nieduże wymiary oraz kształt litery „L” pozwalają na postawienie domku na praktycznie każdej działce. Moduł dostępny w wersji lewej oraz prawej (w odbiciu lustrzanym). ',
+        minPrice: 66000,
+        photos: []
+    },
+    {
+        id: 'pr3',
+        yardage: '70m2',
+        name: 'Dom Z',
+        description: 'Moduł w kształcie litery „Z” to połączenie czterech modułów umożliwiające uzyskanie dowolnego metrażu oraz zagospodarowania budynku jako jednorodzinny lub wielorodzinny obiekt. Poprzez przesunięcia modułów mamy możliwość dopasowania go do działki oraz potrzeb nawet najbardziej wymagających osób.',
+        minPrice: 99999,
+        photos: []
+    },
+    {
+        id: 'pr4',
+        yardage: '+70m2',
+        name: 'Dom niestandardowy',
+        description: 'Jesteśmy w stanie zbudowac dla Ciebie niestandadowy dom. Wybierasz wyposażenie, rozkład i rodzaj, a my zajmujemy się resztą ',
+        minPrice: 100000,
+        photos: []
+
+    },
+
+]
